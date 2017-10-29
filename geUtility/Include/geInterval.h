50859d3ac7f5a24280046cb5aa561c2dbc0d1099
NeonGE <samuel.prince.quezada@gmail.com>
2017-10-29 08:46:15 -0600

    Added OrientedBox and Interval classes

---
+++ geUtility/Include/geInterval.h
@@ -0,0 +1,207 @@
+/*****************************************************************************/
+/**
+ * @file    geInterval.h
+ * @author  Samuel Prince (samuel.prince.quezada@gmail.com)
+ * @date    2017/10/29
+ * @brief   Template for numeric interval.
+ *
+ * Template for numeric interval.
+ *
+ * @bug     No known bugs.
+ */
+/*****************************************************************************/
+#pragma once
+
+/*****************************************************************************/
+/**
+ * Includes
+ */
+/*****************************************************************************/
+#include "gePrerequisitesUtil.h"
+#include <limits>
+
+namespace geEngineSDK {
+  using std::numeric_limits;
+
+  /**
+   * @brief Template for numeric interval
+   */
+  template<typename ElementType>
+  struct Interval
+  {
+   public:
+    /**
+     * @brief Default constructor.
+     * The interval is invalid
+     */
+    Interval()
+      : min(numeric_limits<ElementType>::max()),
+        max(numeric_limits<ElementType>::min())
+    {}
+
+    /**
+     * @brief Creates and initializes a new interval with the specified lower
+     *        and upper bounds.
+     * @param InMin The lower bound of the constructed interval.
+     * @param InMax The upper bound of the constructed interval.
+     */
+    Interval(ElementType InMin, ElementType InMax)
+      : min(InMin),
+        max(InMax)
+    {}
+
+   public:
+    /**
+     * @brief Offset the interval by adding X.
+     * @param X The offset.
+     */
+    void
+    operator+=(ElementType X) {
+      if (IsValid()) {
+        min += X;
+        max += X;
+      }
+    }
+
+    /**
+     * @brief Offset the interval by subtracting X.
+     * @param X The offset.
+     */
+    void
+    operator-=(ElementType X) {
+      if (IsValid()) {
+        min -= X;
+        max -= X;
+      }
+    }
+
+   public:
+    /**
+     * @brief Computes the size of this interval.
+     * @return Interval size.
+     */
+    ElementType
+    size() const {
+      return (max - min);
+    }
+
+    /**
+     * @brief Whether interval is valid (min <= max).
+     * @return false when interval is invalid, true otherwise
+     */
+    ElementType
+    isValid() const {
+      return (min <= max);
+    }
+
+    /**
+     * @brief Checks whether this interval contains the specified element.
+     * @param Element The element to check.
+     * @return true if the range interval the element, false otherwise.
+     */
+    bool
+    contains(const ElementType& Element) const {
+      return isValid() && (Element >= min && Element <= max);
+    }
+
+    /**
+     * @brief Expands this interval to both sides by the specified amount.
+     * @param ExpandAmount The amount to expand by.
+     */
+    void
+    expand(ElementType ExpandAmount) {
+      if (isValid()) {
+        min -= ExpandAmount;
+        max += ExpandAmount;
+      }
+    }
+
+    /**
+     * @brief Expands this interval if necessary to include the specified element.
+     * @param X The element to include.
+     */
+    void
+    include(ElementType X) {
+      if (!isValid()) {
+        min = X;
+        max = X;
+      }
+      else {
+        if (X < min) {
+          min = X;
+        }
+        if (X > max) {
+          max = X;
+        }
+      }
+    }
+
+    /**
+     * @brief Interval interpolation
+     * @param Alpha interpolation amount
+     * @return interpolation result
+     */
+    ElementType
+    interpolate(float Alpha) const {
+      if (IsValid()) {
+        return min + ElementType(Alpha * size());
+      }
+
+      return ElementType();
+    }
+
+   public:
+    /**
+     * @brief Calculates the intersection of two intervals.
+     * @param A The first interval.
+     * @param B The second interval.
+     * @return The intersection.
+     */
+    friend Interval
+    intersect(const Interval& A, const Interval& B) {
+      if (A.isValid() && B.isValid()) {
+        return Interval(Math::max(A.min, B.min), Math::min(A.max, B.max));
+      }
+
+      return Interval();
+    }
+
+   public:
+    /**
+     * @brief Holds the lower bound of the interval.
+     */
+    ElementType min;
+
+    /**
+     * @brief Holds the upper bound of the interval.
+     */
+    ElementType max;
+  };
+
+  /*
+   * Default intervals for built-in types
+   */
+
+#define DEFINE_INTERVAL_WRAPPER_STRUCT(Name, ElementType)                     \
+  struct Name : Interval<ElementType>                                         \
+  {                                                                           \
+   private:                                                                   \
+    typedef Interval<ElementType> super;                                      \
+                                                                              \
+   public:                                                                    \
+    Name() : super() {}                                                       \
+                                                                              \
+    Name(const super& Other) : super(Other) {}                                \
+                                                                              \
+    Name(ElementType InMin, ElementType InMax) : super(InMin, InMax) {}       \
+                                                                              \
+    friend Name                                                               \
+    intersect(const Name& A, const Name& B) {                                 \
+      return intersect(static_cast<const super&>(A),                          \
+                       static_cast<const super&>(B));                         \
+    }                                                                         \
+  };
+
+  DEFINE_INTERVAL_WRAPPER_STRUCT(FloatInterval, float)
+  DEFINE_INTERVAL_WRAPPER_STRUCT(Int32Interval, int32)
+}
---
+++ geUtility/Include/geOrientedBox.h
@@ -0,0 +1,147 @@
+/*****************************************************************************/
+/**
+ * @file    geOrientedBox.h
+ * @author  Samuel Prince (samuel.prince.quezada@gmail.com)
+ * @date    2017/10/29
+ * @brief   Class for arbitrarily oriented boxes (not necessarily axis-aligned)
+ *
+ * Class for arbitrarily oriented boxes (not necessarily axis-aligned).
+ *
+ * @bug     No known bugs.
+ */
+/*****************************************************************************/
+#pragma once
+
+/*****************************************************************************/
+/**
+ * Includes
+ */
+/*****************************************************************************/
+#include "gePrerequisitesUtil.h"
+#include "geVector3.h"
+#include "geInterval.h"
+
+namespace geEngineSDK {
+  class OrientedBox
+  {
+   public:
+    /**
+     * @brief Default constructor.
+     * Constructs a unit-sized, origin-centered box with axes aligned to the
+     * coordinate system.
+     */
+    OrientedBox()
+      : m_center(0.0f),
+        m_axisX(1.0f, 0.0f, 0.0f),
+        m_axisY(0.0f, 1.0f, 0.0f),
+        m_axisZ(0.0f, 0.0f, 1.0f),
+        m_extentX(1.0f),
+        m_extentY(1.0f),
+        m_extentZ(1.0f)
+    {}
+
+   public:
+    /**
+     * @brief Fills in the Verts array with the eight vertices of the box.
+     * @param Verts The array to fill in with the vertices.
+     */
+    FORCEINLINE void
+    calcVertices(Vector3* Verts) const;
+
+    /**
+     * @brief Finds the projection interval of the box when projected onto Axis.
+     * @param Axis The unit vector defining the axis to project the box onto.
+     */
+    FORCEINLINE FloatInterval
+    project(const Vector3& Axis) const;
+
+   public:
+    /**
+     * @brief Holds the center of the box.
+     */
+    Vector3 m_center;
+
+    /**
+     * @brief Holds the x-axis vector of the box. Must be a unit vector.
+     */
+    Vector3 m_axisX;
+
+    /**
+     * @brief Holds the y-axis vector of the box. Must be a unit vector.
+     */
+    Vector3 m_axisY;
+
+    /**
+     * @brief Holds the z-axis vector of the box. Must be a unit vector.
+     */
+    Vector3 m_axisZ;
+
+    /**
+     * @brief Holds the extent of the box along its x-axis.
+     */
+    float m_extentX;
+
+    /**
+     * @brief Holds the extent of the box along its y-axis.
+     */
+    float m_extentY;
+
+    /**
+     * @brief Holds the extent of the box along its z-axis.
+     */
+    float m_extentZ;
+  };
+
+  /***************************************************************************/
+  /**
+   * OrientedBox inline functions
+   */
+  /***************************************************************************/
+
+  FORCEINLINE void
+  OrientedBox::calcVertices(Vector3* Verts) const {
+    static const float Signs[] = { -1.0f, 1.0f };
+
+    for (int32 i = 0; i < 2; ++i) {
+      for (int32 j = 0; j < 2; ++j) {
+        for (int32 k = 0; k < 2; ++k) {
+          *Verts++ = m_center + Signs[i] * m_axisX * m_extentX +
+                                Signs[j] * m_axisY * m_extentY +
+                                Signs[k] * m_axisZ * m_extentZ;
+        }
+      }
+    }
+  }
+
+  FORCEINLINE FloatInterval
+  OrientedBox::project(const Vector3& Axis) const {
+    static const float Signs[] = { -1.0f, 1.0f };
+
+    //Calculate the projections of the box center and the extent-scaled axes.
+    float ProjectedCenter = Axis | m_center;
+    float ProjectedAxisX = Axis | (m_extentX * m_axisX);
+    float ProjectedAxisY = Axis | (m_extentY * m_axisY);
+    float ProjectedAxisZ = Axis | (m_extentZ * m_axisZ);
+
+    FloatInterval ProjectionInterval;
+
+    for (int32 i = 0; i < 2; ++i) {
+      for (int32 j = 0; j < 2; ++j) {
+        for (int32 k = 0; k < 2; ++k) {
+          //Project the box vertex onto the axis.
+          float ProjectedVertex = ProjectedCenter + Signs[i] * ProjectedAxisX +
+                                                    Signs[j] * ProjectedAxisY +
+                                                    Signs[k] * ProjectedAxisZ;
+
+          //If necessary, expand the projection interval to include the box
+          //vertex projection.
+          ProjectionInterval.include(ProjectedVertex);
+        }
+      }
+    }
+
+    return ProjectionInterval;
+  }
+
+  GE_ALLOW_MEMCPY_SERIALIZATION(OrientedBox);
+}
