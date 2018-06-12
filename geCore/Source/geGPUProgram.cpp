d881ac27cd52e6f2a62e730523582788973ccc0e
SAMUEL-LAPTOP\NeonGE <samuel.prince.quezada@gmail.com>
2018-06-12 13:07:13 -0500

    WIP: GPU Programs Fixed style

--- geCore/Source/geGPUProgram.cpp
+++ geCore/Source/geGPUProgram.cpp
@@ -29,101 +29,107 @@
 namespace geEngineSDK {
   using std::static_pointer_cast;
 
-  GPUProgramBytecode::~GPUProgramBytecode()
-  {
-    if (instructions.data)
+  GPUProgramBytecode::~GPUProgramBytecode() {
+    if (instructions.data) {
       ge_free(instructions.data);
+    }
   }
 
-  RTTITypeBase* GPUProgramBytecode::getRTTIStatic()
-  {
+  RTTITypeBase*
+  GPUProgramBytecode::getRTTIStatic() {
     return GPUProgramBytecodeRTTI::instance();
   }
 
-  RTTITypeBase* GPUProgramBytecode::getRTTI() const
-  {
+  RTTITypeBase*
+  GPUProgramBytecode::getRTTI() const {
     return GPUProgramBytecode::getRTTIStatic();
   }
 
   GPUProgram::GPUProgram(const GPU_PROGRAM_DESC& desc)
-    : mNeedsAdjacencyInfo(desc.requiresAdjacency), mLanguage(desc.language), mType(desc.type)
-    , mEntryPoint(desc.entryPoint), mSource(desc.source)
-  {
+    : m_needsAdjacencyInfo(desc.requiresAdjacency),
+      m_language(desc.language),
+      m_type(desc.type),
+      m_entryPoint(desc.entryPoint),
+      m_source(desc.source)
+  {}
 
-  }
-
-  bool GPUProgram::isCompiled() const
-  {
+  bool
+  GPUProgram::isCompiled() const {
     return getCore()->isCompiled();
   }
 
-  String GPUProgram::getCompileErrorMessage() const
-  {
+  String
+  GPUProgram::getCompileErrorMessage() const {
     return getCore()->getCompileErrorMessage();
   }
 
-  SPtr<GPUParamDesc> GPUProgram::getParamDesc() const
-  {
+  SPtr<GPUParamDesc>
+  GPUProgram::getParamDesc() const {
     return getCore()->getParamDesc();
   }
 
-  SPtr<geCoreThread::GPUProgram> GPUProgram::getCore() const
-  {
+  SPtr<geCoreThread::GPUProgram>
+  GPUProgram::getCore() const {
     return static_pointer_cast<geCoreThread::GPUProgram>(m_coreSpecific);
   }
 
-  SPtr<geCoreThread::CoreObject> GPUProgram::createCore() const
-  {
+  SPtr<geCoreThread::CoreObject>
+  GPUProgram::createCore() const {
     GPU_PROGRAM_DESC desc;
-    desc.source = mSource;
-    desc.entryPoint = mEntryPoint;
-    desc.language = mLanguage;
-    desc.type = mType;
-    desc.requiresAdjacency = mNeedsAdjacencyInfo;
-    desc.bytecode = mBytecode;
+    desc.source = m_source;
+    desc.entryPoint = m_entryPoint;
+    desc.language = m_language;
+    desc.type = m_type;
+    desc.requiresAdjacency = m_needsAdjacencyInfo;
+    desc.bytecode = m_bytecode;
 
     return geCoreThread::GPUProgramManager::instance().createInternal(desc);
   }
 
-  SPtr<GPUProgram> GPUProgram::create(const GPU_PROGRAM_DESC& desc)
-  {
+  SPtr<GPUProgram>
+  GPUProgram::create(const GPU_PROGRAM_DESC& desc) {
     return GPUProgramManager::instance().create(desc);
   }
 
-  /************************************************************************/
-  /* 								SERIALIZATION                      		*/
-  /************************************************************************/
-  RTTITypeBase* GPUProgram::getRTTIStatic()
-  {
+  /***************************************************************************/
+  /**
+  * Includes
+  */
+  /***************************************************************************/
+  RTTITypeBase*
+  GPUProgram::getRTTIStatic() {
     return GPUProgramRTTI::instance();
   }
 
-  RTTITypeBase* GPUProgram::getRTTI() const
-  {
+  RTTITypeBase*
+  GPUProgram::getRTTI() const {
     return GPUProgram::getRTTIStatic();
   }
 
-  namespace geCoreThread
-  {
-    GPUProgram::GPUProgram(const GPU_PROGRAM_DESC& desc, GPU_DEVICE_FLAGS::E deviceMask)
-      : mNeedsAdjacencyInfo(desc.requiresAdjacency), mType(desc.type), mEntryPoint(desc.entryPoint), mSource(desc.source)
-      , mBytecode(desc.bytecode)
-    {
-      mParametersDesc = ge_shared_ptr_new<GPUParamDesc>();
+  namespace geCoreThread {
+    GPUProgram::GPUProgram(const GPU_PROGRAM_DESC& desc,
+                           GPU_DEVICE_FLAGS::E deviceMask)
+      : m_needsAdjacencyInfo(desc.requiresAdjacency),
+        m_type(desc.type),
+        m_entryPoint(desc.entryPoint),
+        m_source(desc.source),
+        m_bytecode(desc.bytecode) {
+      m_parametersDesc = ge_shared_ptr_new<GPUParamDesc>();
     }
 
-    bool GPUProgram::isSupported() const
-    {
+    bool
+    GPUProgram::isSupported() const {
       return true;
     }
 
-    SPtr<GPUProgram> GPUProgram::create(const GPU_PROGRAM_DESC& desc, GPU_DEVICE_FLAGS::E deviceMask)
-    {
+    SPtr<GPUProgram>
+    GPUProgram::create(const GPU_PROGRAM_DESC& desc,
+                       GPU_DEVICE_FLAGS::E deviceMask) {
       return GPUProgramManager::instance().create(desc, deviceMask);
     }
 
-    SPtr<GPUProgramBytecode> GPUProgram::compileBytecode(const GPU_PROGRAM_DESC& desc)
-    {
+    SPtr<GPUProgramBytecode>
+    GPUProgram::compileBytecode(const GPU_PROGRAM_DESC& desc) {
       return GPUProgramManager::instance().compileBytecode(desc);
     }
   }
